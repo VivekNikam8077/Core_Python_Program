3d9fafda5220f8be286ca944c8a839f612d86a2b
Vivek Nikam <98576381+VivekNikam8077@users.noreply.github.com>
2025-02-21 12:27:53 +0530

    Add files via upload
    Learned about tupels in Python

---
+++ Day12/assignment1.py
@@ -0,0 +1,10 @@
+# Define the tuple
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+
+# Use the count() method to count occurrences of 4
+count_4 = tuplex.count(4)
+
+# Print the result
+print("Number of times 4 appears in the tuple:", count_4)
+
+# Output: 3
---
+++ Day12/assignment2.py
@@ -0,0 +1,10 @@
+# Define the list
+listx = [5, 10, 7, 4, 15, 3]
+
+# Convert the list to a tuple
+tuplex = tuple(listx)
+
+# Print the result
+print("Converted tuple:", tuplex)
+
+# Output: (5, 10, 7, 4, 15, 3)
